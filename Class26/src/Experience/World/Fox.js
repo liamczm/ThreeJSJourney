ae13e5f1c26a93d08fb38927968aef8b62d3214a
liamczm <loxlan.chenzimian@gmail.com>
2023-09-14 23:24:19 +0800

    0914mac

---
+++ Class26/src/Experience/World/Fox.js
@@ -0,0 +1,29 @@
+import Experience from '../Experience';
+import * as THREE from 'three';
+
+export default class Fox
+{
+    constructor()
+    {
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+        this.resources = this.experience.resources;
+
+        this.resource = this.resources.items.foxModel
+
+        this.setModel()
+    }
+    setModel()
+    {
+        this.model = this.resource.scene;
+        this.model.scale.set(0.02,0.02,0.02);
+        this.scene.add(this.model)
+
+        this.model.traverse(child => {
+            if(child instanceof THREE.Mesh)
+            {
+                child.castShadow = true;
+            }
+        })
+    }
+}
--- Class26/src/Experience/World/World.js
+++ Class26/src/Experience/World/World.js
@@ -2,6 +2,7 @@ import Experience from "../Experience";
 import Environment from "./Environment";
 import * as THREE from 'three';
 import Floor from "./Floor";
+import Fox from "./Fox";
 
 export default class World
 {
@@ -11,18 +12,20 @@ export default class World
         this.scene = this.experience.scene
         this.resources = this.experience.resources
 
-        // Test Mesh
-        const testMesh = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshStandardMaterial()
-        )
-        this.scene.add(testMesh)
+        // // Test Mesh
+        // const testMesh = new THREE.Mesh(
+        //     new THREE.BoxGeometry(1, 1, 1),
+        //     new THREE.MeshStandardMaterial()
+        // )
+        // this.scene.add(testMesh)
 
         this.resources.on('ready',()=>
         {
             //Setup,所有模型加载完成后再初始化
             this.floor = new Floor()
+            this.fox = new Fox()
             this.environment = new Environment()
+            
             
         })
 
