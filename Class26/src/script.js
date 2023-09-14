3300284cbea106c71ada76b828fdf04fa4d0e8d1
Hendrixx <loxlan.chenzimian@gmail.com>
2023-09-14 17:31:16 +0800

    Class26

--- Class26/src/script.js
+++ Class26/src/script.js
@@ -1,6 +1,7 @@
-import test from './test.js'
-test()
+import './style.css'
+import Experience from './Experience/Experience.js'
 
+const experience = new Experience(document.querySelector('canvas.webgl'))
 // import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // import * as dat from 'lil-gui'
